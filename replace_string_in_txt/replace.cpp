967f3b1e28867fc744963cabd214d0f88a2b53e4
AniSkyWorker <holyrecycler@mail.ru>
2016-02-17 00:42:26 +0300

    -renamed some functions -deleted sturct implementation -upgraded some error-checks -replaced some links by const-links in fuctions arguments

--- replace_string_in_txt/replace.cpp
+++ replace_string_in_txt/replace.cpp
@@ -6,66 +6,54 @@
 
 const std::streampos c_maxPos = 2147483648;
 
-struct ReplaceInterface
+bool IsFileSizeIncorrect(const std::string& fileName)
 {
-	bool IsFileSizeIncorrect(std::string& fileName)
+	std::fstream file(fileName, std::ios::in);
+	file.seekg(0, std::ios::end);
+
+	if (file.tellg() > c_maxPos)
 	{
-		std::fstream file(fileName, std::ios::in);
-		file.seekg(0, std::ios::end);
-
-		if (file.tellg() > c_maxPos)
-		{
-			std::cout << "Error: Invalid size of input file (> 2Gb)";
-			return true;
-		}
-
-		return false;
-	}
-
-	bool IsFileNameIncorrect(std::string& inputFileName)
-	{
-		std::ifstream inputFile(inputFileName);
-		if (!inputFile.is_open())
-		{
-			std::cout << "Error: Invalid name of input file!";
-			return true;
-		}
-
-		return false;
-	}
-
-	bool replace(std::string inputFileName, std::string outputFileName,
-		const std::string& oldSubstring, const std::string& newSubstring)
-	{
-		if (IsFileSizeIncorrect(inputFileName))
-			return false;
-
-		if (IsFileNameIncorrect(inputFileName))
-			return false;
-
-		std::ifstream inputFile(inputFileName);
-		std::ofstream outputFile(outputFileName);
-		std::string workString;
-
-		while (!inputFile.eof())
-		{
-			std::getline(inputFile, workString);
-
-			if (workString.size() != 0)
-				boost::replace_all(workString, oldSubstring, newSubstring);
-
-			if (inputFile.peek() != EOF)
-				workString += "\n";
-
-			outputFile << workString;
-		}
-
-		if (!outputFile.flush())
-			std::cout << "Error: Failed writing to output file!";
-
+		std::cout << "Error: Invalid size of input file (> 2Gb)";
 		return true;
 	}
-};
+
+	return false;
+}
+
+bool ReplaceSubstringInFile(const std::string& inputFileName,const std::string& outputFileName,
+	const std::string& oldSubstring, const std::string& newSubstring)
+{
+
+	std::ifstream inputFile(inputFileName);
+
+	if (!inputFile.is_open())
+	{
+		std::cout << "Error: Invalid name of input file!";
+		return false;
+	}
+
+	if (IsFileSizeIncorrect(inputFileName))
+		return false;
+
+	std::ofstream outputFile(outputFileName);
+	std::string workString;
+
+	while (std::getline(inputFile, workString))
+	{
+		if (workString.size() != 0)
+			boost::replace_all(workString, oldSubstring, newSubstring);
+
+		if (inputFile.peek() != EOF)
+			workString += "\n";
+
+		outputFile << workString;
+	}
+
+	if (!outputFile.flush())
+		std::cout << "Error: Failed writing to output file!";
+
+	return true;
+}
 
 int main (int argc, char *argv[])
 {
@@ -76,10 +64,8 @@ int main (int argc, char *argv[])
 		return 1;
 	}
 
-	ReplaceInterface replaceInterface;
-	if(!replaceInterface.replace(argv[1], argv[2], argv[3], argv[4]))
+	if(!ReplaceSubstringInFile(argv[1], argv[2], argv[3], argv[4]))
 		return 1;
 
     return 0;
 }
-
---
+++ tests/readme.txt
@@ -0,0 +1,3 @@
+ReadMe for using program tests:
+1. peplace test.bat and folder "toutputs" into folder with program build
+2. run test.bat
